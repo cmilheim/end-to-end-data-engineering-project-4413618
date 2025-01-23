7617287462702091b48b15dbdcd1bd669719750c
Chris Milheim <milheim05@gmail.com>
2025-01-23 11:04:38 -0600

    Add hourly schedule

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,5 +1,17 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules, ScheduleDefinition, define_asset_job, AssetSelection
 
 from .assets import resources
 
-defs = Definitions(assets=load_assets_from_modules([assets]), resources=resources)
+big_star_job = define_asset_job("big_star_job", selection=AssetSelection.all())
+
+big_star_schedule = ScheduleDefinition(
+    job=big_star_job,
+    cron_schedule="0 * * * *", # @hourly
+)
+
+defs = Definitions(
+    assets=load_assets_from_modules([assets]), 
+    resources=resources,
+    jobs=[big_star_job],
+    schedules=[big_star_schedule]
+)
